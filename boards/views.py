a109e6cb641a67c440573ff1694c84401dc54332
Oliver Schloesser <o.schloesser@plan-net.com>
2021-05-26 18:29:43 +0200

    shor boards on home view without template

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse("Hello World")
+    boards = Board.objects.all()
+    boards_names = list()  # instantiate list of board names
+
+    for board in boards:
+        boards_names.append(board.name)  # append the name of the current board to the list of boards
+
+    response_html = '<br>'.join(boards_names)  # join all items together separated by <br>
+
+    return HttpResponse(response_html)
